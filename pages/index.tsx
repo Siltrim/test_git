7e817e68a00e57c588549372cc2d7484fca8e308
Siltrim <rojerr410@gmail.com>
2024-02-20 12:35:29 +0600

    login fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
 
 export default function Home() {
   return (
 
-
-    <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>Hello</div>
-      <h1>login</h1>
-      <h2>login 1</h2>
+      <>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Hello</div>
+          <h1>login</h1>
+          <h2>login 1</h2>
+      </>
 
 
   );
