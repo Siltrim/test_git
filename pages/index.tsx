160dae52a5a860f541a87a141484d5f12c8b1560
Siltrim <rojerr410@gmail.com>
2024-02-21 17:14:41 +0600

    multi commit css

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
           <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Sign Up</div>
           <div>Multi</div>
           <div>Commit</div>
+          <div>Commit 2</div>
       </>
 
 
