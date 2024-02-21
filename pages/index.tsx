3bd15a45cb320a8e41efe13494b0f3a5c06951f7
Siltrim <rojerr410@gmail.com>
2024-02-21 15:59:08 +0600

    signup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-
-
-
-
 export default function Home() {
   return (
 
       <>
-          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Hello</div>
-          <h1>login</h1>
-          <h2>login 1</h2>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Sign Up</div>
+
       </>
 
 
