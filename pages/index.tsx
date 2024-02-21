ab2a1f0a001c45a07b8799ab02ef586b3998229a
Siltrim <rojerr410@gmail.com>
2024-02-21 17:11:48 +0600

    multi commit 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <>
           <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Sign Up</div>
           <div>Multi</div>
+          <div>Commit</div>
       </>
 
 
